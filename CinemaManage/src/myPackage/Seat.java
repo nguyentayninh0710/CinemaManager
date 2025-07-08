3f8c1c45381c9bb6b4509b9d481117e75d423ceb
phuong3010 <floranguyen@Floras-MacBook-Pro.local>
2025-07-08 21:36:44 +0700

    cinema

---
+++ CinemaManage/src/myPackage/Seat.java
@@ -0,0 +1,41 @@
+package myPackage;
+
+import java.util.ArrayList;
+
+public class Seat {
+	private boolean reserved;
+	private String customerName;
+	private ArrayList<String> items;
+	public Seat() {
+		this.reserved = false;
+		this.customerName = "";
+		this.items = new ArrayList<>();
+	}
+	
+	public void reserve(String name) {
+		this.reserved = true;
+		this.customerName = name;
+	}
+	
+	public void cancelReservation() {
+		this.reserved = false; 
+		this.customerName = "";
+		this.items.clear(); 
+	}
+	
+	public boolean isReserved() {
+		return this.reserved;
+	}
+	
+	public void addItem(String item) {
+		this.items.add(item);
+	}
+	
+	public String toString() {
+		if (this.reserved = false) {
+			return "X" + customerName;
+		} else {
+			return "0";
+		}
+	}
+}
