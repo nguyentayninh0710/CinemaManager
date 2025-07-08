3486b3fe7e6a507c09f4f4f8f35678fae4228830
Huynh Ky Khoi Nguyen <huynhkykhoinguyen0710@gmail.com>
2025-07-08 21:50:33 +0700

    add code

--- CinemaManage/src/myPackage/Cinema.java
+++ CinemaManage/src/myPackage/Cinema.java
@@ -1,5 +1,14 @@
 package myPackage;
 
 public class Cinema {
-
+	private Seat[][] seats;
+	
+	public Cinema(int rows, int cols) {
+		seats = new Seat[rows][cols];
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				seats[i][j] = new Seat();
+			}
+		}
+	}
 }
